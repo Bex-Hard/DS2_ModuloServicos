f9057b505900929b5556898a6ef7e338b6e5377e
Rebecca Hardman <rebeccahardman@gmail.com>
2024-05-31 18:35:10 -0300

    criação da branch de release

---
+++ modulo-servico/src/test/java/org/jala/moduloservico/controller/ViewHistoricoServicoControllerTest.java
@@ -0,0 +1,12 @@
+package org.jala.moduloservico.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ViewHistoricoServicoControllerTest {
+
+    @Test
+    void initialize() {
+    }
+}
